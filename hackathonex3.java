6716e0626058e69bf13f706d1cde343ba0873883
NguyenTrung14 <nguyentrung27092006@gmail.com>
2026-02-25 15:49:47 +0700

    Update hackathonex3.java

--- hackathonex3.java
+++ hackathonex3.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class hackathonex3 {
     public static void main(String[] args) {
-
-        int[] A = {1, 2, 2, 3, 5};
-        int[] B = {2, 5, 6};
-
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Moi ban nhap so luong phan tu mang A: ");
+        int n=sc.nextInt();
+        int[] A = new int[n];
+        for(int i=0;i<n;i++){
+            A[i]=sc.nextInt();
+        }
+        System.out.print("Moi ban nhap so luong phan tu mang B: ");
+        int m=sc.nextInt();
+        int[] B = new int[n];
+        for(int i=0;i<m;i++){
+            B[i]=sc.nextInt();
+        }
         for (int i = 0; i < A.length; i++) {
             int count = 0;
             for (int j = 0; j < A.length; j++) {
@@ -22,5 +33,6 @@ public class hackathonex3 {
                 System.out.print(A[i] + " ");
             }
         }
+        sc.close();
     }
 }
